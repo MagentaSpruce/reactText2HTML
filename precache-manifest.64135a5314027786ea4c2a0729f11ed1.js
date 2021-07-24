7fd27f54d520d8d26d5fa533504871a8a24b37f2
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-07-24 10:58:34 -0400

    Updates

---
+++ precache-manifest.64135a5314027786ea4c2a0729f11ed1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c228c949f569be3d66eebbe8c25bdfd2",
+    "url": "/reactText2HTML/index.html"
+  },
+  {
+    "revision": "8f86fe8a1dade593050f",
+    "url": "/reactText2HTML/static/css/main.096c084f.chunk.css"
+  },
+  {
+    "revision": "6bbb2575db765f6d65f2",
+    "url": "/reactText2HTML/static/js/2.9d0b99b7.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/reactText2HTML/static/js/2.9d0b99b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f86fe8a1dade593050f",
+    "url": "/reactText2HTML/static/js/main.142ff525.chunk.js"
+  },
+  {
+    "revision": "e186d56b42b20480fba6",
+    "url": "/reactText2HTML/static/js/runtime-main.62f7fc20.js"
+  }
+]);
